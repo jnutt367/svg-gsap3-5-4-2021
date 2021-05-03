3599c36ea45c32fcf5c80d6e57786558d9a8351a
Jason Nutt <jnutt367@gmail.com>
2021-05-03 11:21:23 -0400

    commit with surfer guy move

--- js/gsap.js
+++ js/gsap.js
@@ -13,3 +13,4 @@ let tl = gsap.timeline({
 
 .fromTo("#red-car", { x: -10 }, { duration: 3, x: 350, ease: "elastic.in" })
     .to("#guy", { y: -10 }, 0)
+    .fromTo("#surfer", { x: -200, y: -100 }, { duration: 10, x: 200, ease: "bounce.in", y: -150 }, 0)
