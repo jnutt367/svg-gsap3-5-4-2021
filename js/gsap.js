48deb7642753f2caad6d7b36dbf30846e7cb4ce7
Jason Nutt <jnutt367@gmail.com>
2021-05-03 14:04:52 -0400

    commit for other buildings lights maybe make diff color

--- js/gsap.js
+++ js/gsap.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger)
 
 let tl = gsap.timeline({
     scrollTrigger: {
-        trigger: "#red-car",
+        trigger: ".cls-26-1",
         start: "top center",
         end: "top 100px",
         toggleActions: "restart none none reset"
@@ -16,3 +16,4 @@ let tl = gsap.timeline({
     .fromTo("#surfer", { x: -200, y: -100 }, { duration: 10, x: 200, ease: "bounce.in", y: -150 }, 0)
     .fromTo(".cls-26-1", { fill: "black" }, { fill: "white" }, 3)
     .to("#guy", { duration: 3, x: 100, y: 100 }, 2)
+    .fromTo(".cls-17-1", { fill: "#4a3745" }, { fill: "yellow" }, 1)
