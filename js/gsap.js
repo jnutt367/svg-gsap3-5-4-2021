80b033e5e8e1dfd59dd276ec7260fe9807556a95
Jason Nutt <jnutt367@gmail.com>
2021-05-03 13:51:22 -0400

    commit for light in hotel on

--- js/gsap.js
+++ js/gsap.js
@@ -11,6 +11,8 @@ let tl = gsap.timeline({
     }
 })
 
-.fromTo("#red-car", { x: -10 }, { duration: 3, x: 350, ease: "elastic.in" })
+.fromTo("#red-car", { x: -10 }, { duration: 3, y: -20, x: 1000, ease: "back.in" })
     .to("#guy", { y: -10 }, 0)
     .fromTo("#surfer", { x: -200, y: -100 }, { duration: 10, x: 200, ease: "bounce.in", y: -150 }, 0)
+    .fromTo(".cls-26-1", { fill: "black" }, { fill: "white" }, 3)
+    .to("#guy", { duration: 3, x: 100, y: 100 }, 2)
