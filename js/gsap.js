331af81adac0f2e70a9b965de8ec943ffce89fd5
Jason Nutt <jnutt367@gmail.com>
2021-05-03 14:33:12 -0400

    commit for great surfer

--- js/gsap.js
+++ js/gsap.js
@@ -11,9 +11,9 @@ let tl = gsap.timeline({
     }
 })
 
-.fromTo("#red-car", { x: -10 }, { duration: 3, y: -20, x: 1000, ease: "back.in" })
+.fromTo("#red-car", { x: -10 }, { duration: 3, y: -20, x: 400, ease: "back.in" })
     .to("#guy", { y: -10 }, 0)
-    .fromTo("#surfer", { x: -200, y: -100 }, { duration: 10, x: 200, ease: "bounce.in", y: -150 }, 0)
+    .fromTo("#surfer", { x: 320 }, { duration: 10, x: 330, y: -400, ease: "elastic" }, 0)
     .fromTo(".cls-26-1", { fill: "black" }, { fill: "white" }, 3)
     .to("#guy", { duration: 3, x: 100, y: 100 }, 2)
     .fromTo(".cls-17-1", { fill: "#4a3745" }, { fill: "yellow" }, 1)
