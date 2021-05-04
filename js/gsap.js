21d05aba37e03c60af82a9d16c91f60e9a55a689
Jason Nutt <jnutt367@gmail.com>
2021-05-04 10:06:22 -0400

    commit for new office lights

--- js/gsap.js
+++ js/gsap.js
@@ -16,4 +16,5 @@ let tl = gsap.timeline({
     .fromTo("#surfer", { x: 370, y: -400 }, { duration: 10, x: 600, y: -600, ease: "elastic", repeat: -1 }, 0)
     .fromTo(".cls-26-1", { fill: "black" }, { fill: "white" }, 3)
     .to("#guy", { duration: 3, x: 100, y: 100 }, 2)
-    .fromTo(".cls-17-1", { fill: "#4a3745" }, { fill: "yellow" }, 1)
+    .fromTo(".cls-17-1", { fill: "#4a3745", strokeWidth: "6" }, { stroke: "#5dc2a6", strokeWidth: "6", fill: "yellow" }, 1)
+    .fromTo(".cls-26-2", { fill: "black" }, { fill: "blanchedalmond", opcaity: 0.1 }, +4)
