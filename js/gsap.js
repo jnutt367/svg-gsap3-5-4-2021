8d75f7a6894ca17f82a86cf198b9f63242104c62
Jason Nutt <jnutt367@gmail.com>
2021-05-04 09:31:35 -0400

    commit for responsive surfer only on wide

--- js/gsap.js
+++ js/gsap.js
@@ -13,7 +13,7 @@ let tl = gsap.timeline({
 
 .fromTo("#red-car", { x: -10 }, { duration: 3, y: -20, x: 400, ease: "back.in" })
     .to("#guy", { y: -10 }, 0)
-    .fromTo("#surfer", { x: 320 }, { duration: 10, x: 330, y: -400, ease: "elastic" }, 0)
+    .fromTo("#surfer", { x: 370, y: -400 }, { duration: 10, x: 600, y: -600, ease: "elastic", repeat: -1 }, 0)
     .fromTo(".cls-26-1", { fill: "black" }, { fill: "white" }, 3)
     .to("#guy", { duration: 3, x: 100, y: 100 }, 2)
     .fromTo(".cls-17-1", { fill: "#4a3745" }, { fill: "yellow" }, 1)
